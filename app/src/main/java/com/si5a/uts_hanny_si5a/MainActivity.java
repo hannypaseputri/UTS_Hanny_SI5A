f68e297e1b8fa05e8537c806f69988b7c2687956
hannypaseputri <114385288+hannypaseputri@users.noreply.github.com>
2022-10-31 17:04:51 +0700

    commit
    UTS

--- app/src/main/java/com/si5a/uts_hanny_si5a/MainActivity.java
+++ app/src/main/java/com/si5a/uts_hanny_si5a/MainActivity.java
@@ -5,21 +5,37 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-    private Button btnDaftar;
+    Button btnDaftar;
+    EditText etnama, etnomor_pendaftaraan;
+    Spinner sp_Jalur_Pendaftaran;
+    CheckBox cb_Konfirmasi;
+    
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        etnama = findViewById(R.id.et_nama);
+        etnomor_pendaftaraan = findViewById(R.id.et_nomor_pendaftaran);
+        sp_Jalur_Pendaftaran = findViewById(R.id.sp_Jalur_Pendaftaraan);
+        cb_Konfirmasi = findViewById(R.id.cb_Konfirmasi);
+
+
+
         btnDaftar = findViewById(R.id.btn_daftar);
         btnDaftar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast.makeText(MainActivity.this, "Anda Sudah Terdaftar", Toast.LENGTH_SHORT).show();
+                Toast.makeText(MainActivity.this, "Anda Sudah Terdaftar!", Toast.LENGTH_SHORT).show();
             }
         });
 
