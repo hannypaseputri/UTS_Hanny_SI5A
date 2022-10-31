2de4c8c77fe8f2ac3861a5a556dbe3ca9687072c
hannypaseputri <114385288+hannypaseputri@users.noreply.github.com>
2022-10-31 15:04:54 +0700

    commit
    UTS

---
+++ app/src/main/java/com/si5a/uts_hanny_si5a/MainActivity.java
@@ -0,0 +1,28 @@
+package com.si5a.uts_hanny_si5a;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    private Button btnDaftar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnDaftar = findViewById(R.id.btn_daftar);
+        btnDaftar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "Anda Sudah Terdaftar", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
+    }
+}
